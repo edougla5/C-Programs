e68a44d1f4f07d279b2fb6e04aac6a08bf6a012f
Macbook <macbook@MacBook-Pro.local>
2020-10-06 11:52:50 -0400

    first real

---
+++ test.c
@@ -0,0 +1,17 @@
+/**********************************
+* beginner program testing	  *
+*				  *
+*	author: evan douglas	  *
+*				  *
+**********************************/
+
+#include <stdio.h>
+
+int main() {
+	double length = 2.3;
+	double width = 6.8;
+
+	printf("%f\n", length*width);
+	
+	return(0);
+}
